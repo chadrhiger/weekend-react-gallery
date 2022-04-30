ee86cf6244fced5cf775cf6e5267f77ecb1a4900
Chad Rhiger <csrhig@gmail.com>
2022-04-29 22:53:57 -0500

    initial commit

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,14 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    {id: 1, path: 'public/images/boognish.jpeg', description: 'All hail the mighty Boognish', likes: 0 }, 
+    {id: 2, path: 'public/images/bookaMN.jpeg', description: 'Booka B MN', likes: 0},
+    {id: 3, path: 'public/images/fakeShaggyTooDope.jpeg', description: 'Witness the moment', likes: 0},
+    {id: 4, path: 'public/images/goat_small.jpg', description: 'Goat', likes: 0},
+    {id: 5, path: 'public/images/lilijaEyes.jpeg', description: 'Eyes of Lilija', likes: 0},
+    {id: 6, path: 'public/images/pedals.jpeg', description: 'A box of sound boxes', likes: 0},
+    {id: 7, path: 'public/images/tacoCat.jpeg', description: 'TacoCat', likes: 0},
+    {id: 8, path: 'public/images/trickyLoops.jpeg', description: 'Loops are tricky', likes: 0},
 ];
 
 module.exports = galleryItems;
+
+
